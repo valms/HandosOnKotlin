82f5a83e1cb014cd0457d19c361140f5c1031a5d
Valmar Júnior <valmar.junior1@gmail.com>
2017-05-26 19:55:37 -0300

    Hello World v2

--- src/Hello.kt
+++ src/Hello.kt
@@ -2,6 +2,4 @@
  * Created by valmarjunior on 26/05/17.
  */
 
-fun main(args: Array<String>) {
-    println("Hello World, I'm an Kotlin file :D")
-}
+fun main(args: Array<String>) = println("Hello World, I'm an Kotlin file :D")
