420c43632567549cf1fd4588d9d2699be811681f
Valmar Júnior <valmar.junior1@gmail.com>
2017-05-26 20:02:45 -0300

    editing Copyright disclaimer.

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,5 +1,9 @@
 /**
- * Created by valmarjunior on 26/05/17.
- */
+*
+* This file is subject to the terms and conditions defined in
+* file 'LICENSE.txt', which is part of this source code package.
+*
+* Created by Valmar Junior on 26/05/17.
+**/
 
 fun main(args: Array<String>) = println("Hello World, I'm an Kotlin file :D")
