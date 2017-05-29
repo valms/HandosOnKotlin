762ec62baaae4a7c128f9fd72c785d4e0264e7df
Francisco Valmar Isaias Silva Júnior <valmar.junior1@gmail.com>
2017-05-29 15:48:51 -0300

    Início dos estudos.

--- src/Hello.kt
+++ src/Hello.kt
@@ -2,4 +2,39 @@
  * Created by valmarjunior on 26/05/17.
  */
 
-fun main(args: Array<String>) = println("Hello World, I'm an Kotlin file :D")
+fun main(args: Array<String>) = println("Hello World, I'm an Kotlin file :D");
+
+
+// ------------------------------ Vals and vars ---------------------------------------------------
+/**
+ * O "var" é uma variável mutável (dinâmica, não constante), o que quer dizer,
+ *que a variável pode ser reatribuída.
+ */
+
+var variavelUm = "Kotlin"
+// Obs.: "var" pode ser inicalizado em um momento após a declaração
+var variavelDois: String
+variavelDois = "Kotlin"
+
+var variavelTres = "kotlin"
+variavelTres = "mais kotlin"
+
+/**
+ * O "val" é usado para declarar variáveis de "apenas leitura" => Equivalene ao "final" do Java
+ */
+
+
+val variavelQuatro = "kotlin"
+
+// ---------------------------------------------------------------------------------------------
+
+
+// ------------------------------ Inferência de Tipo --------------------------------------------
+/**
+ * O "var" é uma variável mutável (dinâmica, não constante), o que quer dizer,
+ *que a variável pode ser reatribuída.
+ */
+
+
+
+// ---------------------------------------------------------------------------------------------
